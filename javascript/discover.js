6aef78271b02aa4655d042c73e6b160dd26baa5a
Musika Iqfat <musfikaiqfatmomo21.com>
2025-03-04 14:20:07 +0600

    added another html file

---
+++ javascript/discover.js
@@ -0,0 +1,7 @@
+document.getElementById('query-btn').addEventListener('click',function(){
+    window.location.href = "./main.html";
+});
+
+document.getElementById('back-desk-btn').addEventListener('click',function(){
+    window.location.href = "./home.html";
+});
