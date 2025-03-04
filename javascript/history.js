6599a79ce87618064d08e52d0a5884373a2d204d
Musika Iqfat <musfikaiqfatmomo21.com>
2025-03-04 19:12:49 +0600

    added more function

--- javascript/history.js
+++ javascript/history.js
@@ -12,10 +12,33 @@ for(let button of buttons){
             let updatedNum2 = parseInt(num);
             updatedNum2--;
             document.getElementById('num-6').innerText = updatedNum2;
-            button.style.backgroundColor = "gray";
-            button.style.color = "white";
+            button.style.backgroundColor = "#c3c7c4";
+            button.style.color = "gray";
             button.style.cursor = "not-allowed"; 
 
+
+
+            // Create a new div
+            let newDiv = document.createElement('div');
+
+    // Add text inside the div
+    newDiv.textContent = "You have completed the task";
+
+    // Add styles to the div
+    // newDiv.style.width = "300px";
+    // newDiv.style.height = "100px";
+    newDiv.style.backgroundColor = "#F4F7FF";
+    newDiv.style.color = "black";
+    newDiv.style.fontSize = "18px";
+    newDiv.style.textAlign = "center";
+    newDiv.style.padding = "10px 20px"
+    // newDiv.style.lineHeight = "100px"; // To vertically center the text
+    // newDiv.style.marginTop = "20px";
+    // newDiv.style.border = "2px solid blue";
+    newDiv.style.borderRadius = "10px";
+
+    // Append the new div to the body (or any specific container)
+    document.getElementById('history').appendChild.apply(newDiv);
             
     });
 }
